5d7b8284abf34da4edf217c2ae9dc81c9631c76d
Patrick Poulain <patrick@adopteunmec.com>
2015-08-18 18:04:23 +0200

    Allow to run the app using docker

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM maxexcloo/nginx-php:latest
+MAINTAINER Patrick Poulain <petitchevalroux@free.fr>
+RUN mkdir -p /data/http/public && \
+perl -pi -e 's~root /data/http~root /data/http/public~g' /etc/nginx/host.d/default.conf
+ADD . /data/http
---
+++ bin/start-dev-http.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start the docker container using the repository as volume
+# it allow to edit php file on host and see changes on running container
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+DOCKER_IMAGE=$(docker build . | egrep -o 'Successfully built [a-zA-Z0-9]+' | sed -e 's~Successfully built ~~')
+echo $DOCKER_IMAGE
+
+docker run -d -p 8080:80 -v $DIR:/data/http $DOCKER_IMAGE
