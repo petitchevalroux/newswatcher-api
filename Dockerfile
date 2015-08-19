56c9416c6bd04b09264d097bd18d0ee6e393ebbb
Patrick Poulain <patrick@adopteunmec.com>
2015-08-19 14:07:10 +0200

    Handle production vs development environment using SLIM_MODE variable

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,5 @@ RUN mkdir -p /data/http/public && \
 perl -pi -e 's~root /data/http~root /data/http/public~g' /etc/nginx/host.d/default.conf
 ADD . /data/http
 ADD ./docker/nginx/default-host.conf /etc/nginx/host.d/default.conf
+RUN mkdir -p /data/config
+ADD ./docker/php-fpm/php-env.conf /data/config/php-env.conf
--- src/Dependencies/slim.php
+++ src/Dependencies/slim.php
@@ -1,9 +1,24 @@
 <?php
 
-$app = new \Slim\Slim();
+$slimMode = getenv('SLIM_MODE');
+if ($slimMode === 'development') {
+    $slimMode = 'development';
+    ini_set('display_errors', 1);
+    $debug = true;
+} else {
+    $slimMode = 'production';
+    ini_set('display_errors', 0);
+    $debug = false;
+}
+ini_set('error_reporting', E_ALL);
+
+$app = new \Slim\Slim([
+    'mode' => $slimMode,
+    'debug' => $debug,
+]);
+
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
-
 });
 
 return $app;
