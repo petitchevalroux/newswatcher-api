378c84be5d3a73c72bc2a0be95df6594eea21eb4
Patrick Poulain <petitchevalroux@free.fr>
2015-08-19 14:07:10 +0200

    Handle production vs development environment using SLIM_MODE variable

--- bin/start-dev-http.sh
+++ bin/start-dev-http.sh
@@ -7,4 +7,4 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 DOCKER_IMAGE=$(docker build . | egrep -o 'Successfully built [a-zA-Z0-9]+' | sed -e 's~Successfully built ~~')
 echo $DOCKER_IMAGE
 
-docker run -d -p 8080:80 -v $DIR:/data/http $DOCKER_IMAGE
+docker run -d -p 8080:80 -v $DIR:/data/http -e "SLIM_MODE=development" $DOCKER_IMAGE
