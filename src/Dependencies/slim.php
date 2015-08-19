f36c5882fb4744d96f9bb754f3810d890061bcf7
Patrick Poulain <petitchevalroux@free.fr>
2015-08-19 17:00:29 +0200

    Avoid slim to stop runtime on minor error in production

--- src/Dependencies/slim.php
+++ src/Dependencies/slim.php
@@ -4,13 +4,18 @@ $slimMode = getenv('SLIM_MODE');
 if ($slimMode === 'development') {
     $slimMode = 'development';
     ini_set('display_errors', 1);
+    ini_set('error_reporting', E_ALL);
     $debug = true;
 } else {
     $slimMode = 'production';
     ini_set('display_errors', 0);
+    // As reported in https://github.com/slimphp/Slim/issues/1454#issuecomment-132623409
+    // Slim stop script execution based on error_reporting settings.
+    // It can be dangerous in production
+    ini_set('error_reporting', E_ALL & ~E_STRICT & ~E_WARNING & ~E_NOTICE & E_DEPRECATED);
     $debug = false;
 }
-ini_set('error_reporting', E_ALL);
+
 
 $app = new \Slim\Slim([
     'mode' => $slimMode,
