66bf4d7fbd058b3d6f7938edef0521e994907f54
Patrick Poulain <patrick@adopteunmec.com>
2015-10-17 19:41:13 +0200

    Add more unit test to JsonApiController

--- tests/JsonApiControllerTest.php
+++ tests/JsonApiControllerTest.php
@@ -2,12 +2,10 @@
 
 use Faker\Factory;
 use NwApi\Controllers\JsonApiController;
-use Doctrine\ORM\Mapping\ClassMetadata;
 use NwApi\Di;
 
 class JsonApiControllerTest extends PHPUnit_Framework_TestCase
 {
-
     /**
      * @var Factory
      */
@@ -21,14 +19,13 @@ class JsonApiControllerTest extends PHPUnit_Framework_TestCase
 
     public function testCreateEntity()
     {
-
         $controller = new JsonApiController();
         $object = new NwApi\Entities\User();
         $object->name = $this->faker->userName();
         $object->twitterId = $this->faker->randomDigit();
         Slim\Environment::mock([
             'HTTP_CONTENT_TYPE' => 'application/json',
-            'slim.input' => json_encode($object)
+            'slim.input' => json_encode($object),
         ]);
         $di = Di::getInstance();
         $di->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
@@ -47,4 +44,51 @@ class JsonApiControllerTest extends PHPUnit_Framework_TestCase
         $controller->createEntity($meta);
     }
 
+    public function testDeleteEntity()
+    {
+        $controller = new JsonApiController();
+        $meta = $this->getMockBuilder("Doctrine\ORM\Mapping\ClassMetadata")
+                ->setConstructorArgs(["NwApi\Entities\User"])
+                ->getMock();
+        $di = Di::getInstance();
+        $di->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $di->em->expects($this->any())
+            ->method('find')->willReturn(true);
+        $meta->identifier = ['id'];
+        $controller->deleteEntity($meta, [$this->faker->randomDigit()]);
+    }
+
+    public function testPatchEntity()
+    {
+        $controller = new JsonApiController();
+        $meta = $this->getMockBuilder("Doctrine\ORM\Mapping\ClassMetadata")
+                ->setConstructorArgs(["NwApi\Entities\User"])
+                ->getMock();
+        $di = Di::getInstance();
+        $di->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $di->em->expects($this->any())
+            ->method('find')->willReturn(new NwApi\Entities\User());
+        $meta->identifier = ['id'];
+        $controller->patchEntity($meta, [$this->faker->randomDigit()]);
+    }
+
+    public function testGetEntity()
+    {
+        $controller = new JsonApiController();
+        $meta = $this->getMockBuilder("Doctrine\ORM\Mapping\ClassMetadata")
+                ->setConstructorArgs(["NwApi\Entities\User"])
+                ->getMock();
+        $di = Di::getInstance();
+        $di->em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $di->em->expects($this->any())
+            ->method('find')->willReturn(new NwApi\Entities\User());
+        $meta->identifier = ['id'];
+        $controller->getEntity($meta, [$this->faker->randomDigit()]);
+    }
 }
